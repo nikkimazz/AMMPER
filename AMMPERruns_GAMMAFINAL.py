3e6bea713dc4f5867f0971a05d6f67d4e2c99ce3
DanPalBCM <daniel.palacios@bcm.edu>
2023-07-15 18:00:09 -0500

    Gamma parametrization

--- AMMPERruns_GAMMAFINAL.py
+++ AMMPERruns_GAMMAFINAL.py
@@ -2,19 +2,12 @@ import sys
 import os
 import time
 
-for i in range(2):
+for i in range(3):
 
     print("Sample running: " + str(i))
 
-    # gamma rad51 basic 2.5 Gy
-    os.system("python .\AMMPERBulk_GAMMAfinal.py d b a 2.5 rad51_25")
-
-    # gamma rad51 basic 0 Gy
-    os.system("python .\AMMPERBulk_GAMMAfinal.py d b a 0 rad51_0")
-
-    # gamma rad51 basic 1 Gy
-    os.system("python .\AMMPERBulk_GAMMAfinal.py d b a 30 rad51_300")
-
+    # gamma WT 2.5 Gy
+    os.system("python .\AMMPERBulk_GAMMAfinal.py d a a 2.5 WT_25k50")
 
     print("WAITING 60 seconds now")
     time.sleep(60)
