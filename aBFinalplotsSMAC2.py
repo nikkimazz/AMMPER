9e9f71763b01b90c886470755976c5cb8189d2ea
Pramesh Sharma <prameshsharma25@gmail.com>
2023-12-10 22:15:23 -0800

    Redefined truncation values for rad51 data

--- aBFinalplotsSMAC2.py
+++ aBFinalplotsSMAC2.py
@@ -170,10 +170,11 @@ def ExperimentalConencentrations(data):
     return B_C, P_C, Time
 
 def accuracy_ML(Experimental_B, Predicted_B, Experimental_P, Predicted_P):
-    w, n = 2, 2
+    w, n = 1, 1
     delta_blue = sum((Experimental_B - Predicted_B) ** 2)
     delta_pink = sum((Experimental_P - Predicted_P) ** 2)
     delta_diff = w * abs(delta_blue - delta_pink) ** n
+    # Worth calculating the difference between the Experimental and Predicted (original function) and comparing it to the difference
     # delta2 = w * |delta - delta1| ** n difference between the two regularization term, look regularization terms in ML
     delta_total = delta_blue + delta_pink + delta_diff
     # accuracy = classification_report(Experimental_B,Predicted_B).precision
@@ -196,9 +197,9 @@ def main_DP(name, data1, data1_std):
             ### Testing
             # Bi, Pi, t = AlamarblueMechanics(resultsi, [0.9418799810768097, 1.7714680707739503, 0.1977339123715871, 419, 6787, 571195, 0.5], 'k')
 
-            # Bi, Pi, t = AlamarblueMechanics(resultsi, [0.7799990799515666, 1.679928455577914, 0.10002078747628415, 450, 6601, 9994, 0.5], 'k')
+            Bi, Pi, t = AlamarblueMechanics(resultsi, [0.7799990799515666, 1.679928455577914, 0.10002078747628415, 450, 6601, 9994, 0.5], 'k')
 
-            Bi, Pi, t = AlamarblueMechanics(resultsi, [0.9997875025037495, 0.8092700826448992, 0.7249542604694886, 574, 662, 9934, 0.5], 'k')
+            # Bi, Pi, t = AlamarblueMechanics(resultsi, [1.3558767947842083, 0.9958129817752762, 0.13882892817316034, 909, 753, 3795, 0.5], 'k')
             
 
             B_m.append(Bi)
@@ -227,13 +228,16 @@ def main_DP(name, data1, data1_std):
     colorb = '#42329a'
     colorp = '#e54da7'
 
-    B_C, P_C, Time = ExperimentalConencentrations(data1.head(TRUNCATED))
+    # B_C, P_C, Time = ExperimentalConencentrations(data1.head(TRUNCATED_FINAL))
+    B_C, P_C, Time = ExperimentalConencentrations(data1[TRUNCATED_START:TRUNCATED_FINAL])
     B_C = B_C / (B_C[0] + P_C[0])
     P_C = P_C / (B_C[0] + P_C[0])
-    B_Cp, P_Cp, Time = ExperimentalConencentrations(data1p.head(TRUNCATED))
+    # B_Cp, P_Cp, Time = ExperimentalConencentrations(data1p.head(TRUNCATED_FINAL))
+    B_Cp, P_Cp, Time = ExperimentalConencentrations(data1p[TRUNCATED_START:TRUNCATED_FINAL])
     B_Cp = B_Cp / (B_Cp[0] + P_Cp[0])
     P_Cp = P_Cp / (B_Cp[0] + P_Cp[0])
-    B_Cn, P_Cn, Time = ExperimentalConencentrations(data1n.head(TRUNCATED))
+    # B_Cn, P_Cn, Time = ExperimentalConencentrations(data1n.head(TRUNCATED_FINAL))
+    B_Cn, P_Cn, Time = ExperimentalConencentrations(data1n[TRUNCATED_START:TRUNCATED_FINAL])
     B_Cn = B_Cn / (B_Cn[0] + P_Cn[0])
     P_Cn = P_Cn / (B_Cn[0] + P_Cn[0])
     # plt.scatter(Time, B_C, marker = '*', color = colorb, label = 'Experimental Blue')
@@ -277,7 +281,7 @@ def main_DP(name, data1, data1_std):
 
     plt.xlabel('Generation')
     plt.style.use('classic')
-    plt.xlim([0, TRUNCATED])
+    plt.xlim([TRUNCATED_START, TRUNCATED_FINAL])
     plt.xlabel('Time [hrs]')
     plt.title(name + 'aB concentration fractions')
     plt.legend()
@@ -303,9 +307,12 @@ def interpolate_data(B_m, P_m, data1, data1_std, resultsi):
     data1n['Time'] = data1['Time']
 
 #
-    B_C, P_C, Time = ExperimentalConencentrations(data1.head(TRUNCATED))
-    B_Cp, P_Cp, Time = ExperimentalConencentrations(data1p.head(TRUNCATED))
-    B_Cn, P_Cn, Time = ExperimentalConencentrations(data1n.head(TRUNCATED))
+    # B_C, P_C, Time = ExperimentalConencentrations(data1.head(TRUNCATED_FINAL))
+    B_C, P_C, Time = ExperimentalConencentrations(data1[TRUNCATED_START:TRUNCATED_FINAL])
+    # B_Cp, P_Cp, Time = ExperimentalConencentrations(data1p.head(TRUNCATED_FINAL))
+    B_Cp, P_Cp, Time = ExperimentalConencentrations(data1p[TRUNCATED_START:TRUNCATED_FINAL])
+    # B_Cn, P_Cn, Time = ExperimentalConencentrations(data1n.head(TRUNCATED_FINAL))
+    B_Cn, P_Cn, Time = ExperimentalConencentrations(data1n[TRUNCATED_START:TRUNCATED_FINAL])
     #B_Cp, P_Cp, Time = ExperimentalConencentrations(data1_std.head(30))
     # assume After 30 hours sytem remains in equilibrium since model does not make any statement of additional mechanics >><<
     B_C = np.append(B_C, B_C[-1])
@@ -331,7 +338,7 @@ def interpolate_data(B_m, P_m, data1, data1_std, resultsi):
 
     # Data with generation time ******
     Healthy = resultsi.loc[resultsi["Health"] == 1]
-    Generations = np.linspace(0, int(Healthy['Generation'].max()), num=len(Healthy['Generation'].unique()))
+    Generations = np.linspace(TRUNCATED_START, int(Healthy['Generation'].max()), num=len(Healthy['Generation'].unique()))
     # Generation to hrs ->> Conversion
     Generations_t =  Generations * GENCONVER / 60
 
@@ -374,7 +381,8 @@ def plot_loss(iteration_values, loss_values):
     plt.show()
 
 if __name__ == "__main__":
-    TRUNCATED = 15
+    TRUNCATED_START = 4
+    TRUNCATED_FINAL = 48 # end value
     GENCONVER = 198 # 198 min for ion conversion data  205.62 for gamma radation
 
     optimize_parameters = input("Would you like to optimize? [Y/N]: ")
@@ -459,13 +467,10 @@ if __name__ == "__main__":
         
     else:
         # Start value truncation = 0 for WT, but rad51 not zero
-        TRUNCATED = 15 # end value
-        GENCONVER = 198 # 198 min for ion conversion data  205.62 for gamma radation
         namesk = ['Time', 'A570', 'A600', 'A690', 'A750']
 
 
         # Now rad51
-
         datar = pd.read_csv(r"abFinalPlots/AlamarblueRawdatarad51KGy.csv", names = namesk)
         datarSTD = pd.read_csv(r"abFinalPlots/AlamarblueRawdatarad51KGySTD.csv", names = namesk)
 
